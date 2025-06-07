096132f998dbb01c153c2c8f56054c8e21f7d93c
Eduarda Valmor <eduarda.bserrano@gmail.com>
2025-06-07 00:23:24 -0300

    Adicionando pessoas na lista

--- Exercicio1/Main.java
+++ Exercicio1/Main.java
@@ -5,5 +5,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+
+        Pessoa pessoa1 = new Pessoa(20, "Eduarda");
+        Pessoa pessoa2 = new Pessoa(25, "Bruna");
+        Pessoa pessoa3 = new Pessoa(22, "Julia");
+
+        listaDePessoas.add(pessoa1);
+        listaDePessoas.add(pessoa2);
+        listaDePessoas.add(pessoa3);
+
     }
 }
